81ac6234d09aa71e132463822fcc6df033a7700d
Ondrej Machulda <ondrej.machulda@lmc.eu>
2014-09-05 18:27:12 +0200

    Composer.phar removed from the project; but help how to download it is echoed to the user instead

--- bin/steward.php
+++ bin/steward.php
@@ -3,15 +3,30 @@
 
 namespace Lmc\Steward;
 
-require_once 'vendor/autoload.php';
-
 use Symfony\Component\Console\Application;
+
+function requireIfExists($file)
+{
+    if (file_exists($file)) {
+        return require_once $file;
+    }
+}
+
+if (!requireIfExists(__DIR__ . '/../vendor/autoload.php')) {
+    die(
+        'You must set up the project dependencies, run the following commands:' . PHP_EOL .
+        'curl -sS https://getcomposer.org/installer | php' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL
+    );
+}
 
 $application = new Application('Steward', '0.0.1');
 $application
-    ->addCommands([
-        new RunTestsCommand(),
-        new InstallCommand(),
-    ]);
+    ->addCommands(
+        [
+            new RunTestsCommand(),
+            new InstallCommand(),
+        ]
+    );
 
 $application->run();
