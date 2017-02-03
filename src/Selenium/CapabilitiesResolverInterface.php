2b90e13e8355e76f105a4c22098ca611c1afe5f6
Ondrej Machulda <ondrej.machulda@lmc.eu>
2017-02-03 14:16:29 +0100

    Resolve also required capabilities

--- src/Selenium/CapabilitiesResolverInterface.php
+++ src/Selenium/CapabilitiesResolverInterface.php
@@ -5,6 +5,9 @@ use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Lmc\Steward\ConfigProvider;
 use Lmc\Steward\Test\AbstractTestCase;
 
+/**
+ * Resolve WebDriver capabilities to be used for a TestCase
+ */
 interface CapabilitiesResolverInterface
 {
     /**
@@ -13,10 +16,21 @@ interface CapabilitiesResolverInterface
     public function __construct(ConfigProvider $config);
 
     /**
-     * Resolve capabilities for given test
+     * Resolve desired capabilities for given test
      *
      * @param AbstractTestCase $test
      * @return DesiredCapabilities
      */
-    public function resolveCapabilities(AbstractTestCase $test);
+    public function resolveDesiredCapabilities(AbstractTestCase $test);
+
+    /**
+     * Resolve required capabilities for given test.
+     *
+     * Please note "required capabilities" are implemented inconsistently in current WebDriver backends, so you should
+     * most probably set only "desired capabilities" unit backends conforms the W3C Webdriver spec.
+     *
+     * @param AbstractTestCase $test
+     * @return DesiredCapabilities
+     */
+    public function resolveRequiredCapabilities(AbstractTestCase $test);
 }
