0e91d76a56a5dc8bb15d2114b2aeb237b89b8042
Ondrej Machulda <ondrej.machulda@lmc.eu>
2021-03-02 18:34:10 +0000

    Use tagged version of lmc/coding-standard

--- ecs.php
+++ ecs.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1);
 
-use PHP_CodeSniffer\Standards\Generic\Sniffs\Files\LineLengthSniff;
 use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\ForbiddenFunctionsSniff;
 use PhpCsFixer\Fixer\PhpUnit\PhpUnitExpectationFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\CodingStandard\Fixer\LineLength\LineLengthFixer;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -39,8 +39,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $services = $containerConfigurator->services();
 
-    $services->set(LineLengthSniff::class)
-        ->property('absoluteLineLimit', 120);
+    $services->set(LineLengthFixer::class)
+        ->call(
+            'configure',
+            [['line_length' => 120, 'break_long_lines' => true, 'inline_short_lines' => false]]
+        );
 
     $services->set(PhpUnitExpectationFixer::class)
         ->call('configure', [['target' => '5.6']]);
